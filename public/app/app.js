d9bf40649ec585727e984d5c328e8e4039f5f299
James Johnstone <jcjohnstone8@gmail.com>
2015-12-06 15:14:34 +0000

    add initial jade views

---
+++ public/app/app.js
@@ -0,0 +1,17 @@
+﻿angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/', { templateUrl: '/partials/home/main', controller: 'mainController'})
+});
+
+
+angular.module('app').run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+        if (rejection === 'not authorized') {
+            $location.path('/');
+        }
+    });
+});
