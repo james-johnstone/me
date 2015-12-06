4188c7bf4cf35967dacc74744087541d4513dde5
James Johnstone <jcjohnstone8@gmail.com>
2015-12-06 20:51:36 +0000

    add basic header layout

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,6 @@
 ﻿angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function ($routeProvider, $locationProvider) {
-
     $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', { templateUrl: '/partials/home/main', controller: 'mainController'})
