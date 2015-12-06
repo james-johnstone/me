2a143969307a4d013e9dbc2d16478ec17aec2a54
James Johnstone <jcjohnstone8@gmail.com>
2015-12-06 14:27:49 +0000

    add additional dependencies, add express and node config

---
+++ server/config/express.js
@@ -0,0 +1,9 @@
+﻿var express         = require('express')
+    morgan          = require('morgan');
+
+module.exports = function (app, config) {
+    app.set('views', config.rootPath + '/server/views');
+    app.set('view engine', 'jade');
+    app.use(morgan('dev'));
+    app.use(express.static(config.rootPath + 'public'));
+}
---
+++ server/config/routes.js
@@ -0,0 +1,22 @@
+﻿var path                        = require('path');
+
+module.exports = function (app) {
+    //====================================================================
+    // PARTIALS ROUTE
+    //====================================================================
+    app.get('/partials/*', function (req, res) {
+        //res.render(path.normalize(req.url));
+        res.render('partials/' + req.params[0]);
+    });
+
+    //====================================================================
+    // DEFAULT ROUTES
+    //====================================================================
+    app.all('/api/*', function (req, res) {
+        res.send(404);
+    });
+
+    app.get('*', function (req, res) {
+        res.render('index');
+    });
+}
